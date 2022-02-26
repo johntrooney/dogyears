0b74cc403c6a34f108921a724b9b58616841b48d
johntrooney <100171856+johntrooney@users.noreply.github.com>
2022-02-26 15:30:27 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+//my age
+
+const myAge = 33;
+
+//first 2 years of life
+
+var earlyYears = 2 * 10.5;
+
+//age 3 and up
+
+var laterYears = 4 * (myAge - 2);
+
+var dogYears = earlyYears + laterYears;
+
+var myName = 'John'.toLowerCase();
+
+console.log(`My name is John. I am ${myAge} in human years and I am ${dogYears} in dog years.`)
